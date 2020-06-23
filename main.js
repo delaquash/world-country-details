4bfab573583423f423e2a02e532d2936fd856a1e
Olaide <olaidemmanuel1@gmail.com>
2020-06-23 18:23:39 +0100

    Done with the styling

---
+++ main.js
@@ -0,0 +1,39 @@
+const countriesEl = document.getElementById('countries');
+const toggleBtn = document.getElementById('toggle');
+const filterBtn = document.getElementById('filter');
+
+
+
+getCountries();
+async function getCountries() {
+    const res = await fetch('https://restcountries.eu/rest/v2/all');
+    const countries = await res.json();
+
+    displayCountries(countries);
+}
+
+function displayCountries(countries) {
+    countriesEl.innerHTML = '';
+    countries.forEach(country => {
+        const countryEl = document.createElement('div');
+        countryEl.classList.add('country');
+        countryEl.innerHTML = `
+            <div>
+                    <img src='${country.flag}' alt="Germany" />
+            </div>
+            <div class="country-info">
+                    <h2>${country.name}</h2>
+                    <p><strong>Populaton:</strong>${country.population}</p>
+                    <p><strong>Region:</strong>${country.region}</p>
+                    <p><strong>Capital:</strong>${country.capital}</p>
+            </div>
+    `;
+        countriesEl.appendChild(countryEl);
+    });
+}
+toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+});
+filterBtn.addEventListener('click', () => {
+    filterBtn.classList.toggle('open');
+});
